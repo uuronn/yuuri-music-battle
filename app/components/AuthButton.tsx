32d20a00ef992eb47b40c9949f287333f354ec54
uuronn <uuronprogram@gmail.com>
2023-04-01 02:07:37 +0900

    Create: AuthButtonコンポーネントを作成

---
+++ app/components/AuthButton.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { ComponentPropsWithRef } from "react";
+
+interface Props extends ComponentPropsWithRef<"button"> {
+  children: React.ReactNode;
+}
+
+export const AuthButton = ({ children, ...props }: Props): JSX.Element => {
+  return (
+    <button
+      className="pl-3 px-6 py-2 flex items-center gap-2 bg-white rounded-full border-2 border-gray-600 text-xl sm:text-2xl hover:bg-gray-100"
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
