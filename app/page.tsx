3e17a9a9b10dba85f9ed3268be151992683b4229
uuronn <uuronprogram@gmail.com>
2023-04-01 05:41:53 +0900

    オンライン表示の実装

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,22 @@ const Home = (): JSX.Element => {
     { path: "/room3", roomId: 3, joiningList: 0 },
   ];
 
+  const onlineCount: number = 2;
+
   return (
     <main className="h-screen">
-      <Container className="h-3/4 flex justify-center items-center">
+      <Container className="h-full flex justify-center items-center relative">
+        <p
+          className={`text-2xl flex gap-2 absolute top-[32px] right-[16px] before:content-[''] before:block before:rounded-full before:w-4 before:h-4 ${
+            onlineCount >= 2
+              ? "before:bg-green-500"
+              : onlineCount === 1
+              ? "before:bg-yellow-500"
+              : "before:bg-red-500"
+          }`}
+        >
+          オンライン: {onlineCount}人
+        </p>
         <ul className="flex flex-col sm:flex-row items-center justify-center gap-10">
           {ROOM_LIST.map((room) => (
             <li key={room.roomId}>
