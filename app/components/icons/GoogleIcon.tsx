c16e29add66fd4b1fd23ce9ffcf74c26e0d126da
uuronn <uuronprogram@gmail.com>
2023-03-31 23:59:49 +0900

    Create: GoogleIconを作成

---
+++ app/components/icons/GoogleIcon.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export const GoogleIcon = (): JSX.Element => (
+  <Image
+    src="/google.svg"
+    width={32}
+    height={32}
+    alt="Googleの画像"
+    className="sm:w-12 bg-black"
+    priority
+  />
+);
