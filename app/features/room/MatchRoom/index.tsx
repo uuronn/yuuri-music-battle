470f2c98a52873cf1793bf2039af8fc746ab5636
uuronn <uuronprogram@gmail.com>
2023-04-01 05:11:27 +0900

    Create: MatchRoomコンポーネントを作成

---
+++ app/features/room/MatchRoom/index.tsx
@@ -0,0 +1,26 @@
+import { RoomIcon } from "@/app/components/icons/RoomIcon";
+import Link from "next/link";
+
+type Props = {
+  joiningCount: number;
+  path: string;
+  room: number;
+};
+
+export const MatchRoom = ({ joiningCount, path, room }: Props): JSX.Element => {
+  return (
+    <Link
+      href={path}
+      className="inline-block rounded-2xl bg-gray-100 hover:bg-gray-200"
+    >
+      <div className="w-52 p-5 pb-4 sm:w-64 border-2 border-gray-600 rounded-2xl">
+        <p className="text-right">{joiningCount} / 5</p>
+        <RoomIcon className="m-auto mb-3" />
+        <p className="text-xl text-center">
+          ルーム&nbsp;
+          <span className="text-2xl font-medium">{room}</span>
+        </p>
+      </div>
+    </Link>
+  );
+};
