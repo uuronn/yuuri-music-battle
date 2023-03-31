fafdc2cb5dcb095ee865d201ab90ec991124add8
uuronn <uuronprogram@gmail.com>
2023-04-01 05:11:41 +0900

    Fix: indexページを作成

--- app/page.tsx
+++ app/page.tsx
@@ -1,91 +1,31 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "./page.module.css";
+import { Container } from "./components/Container";
+import { MatchRoom } from "./features/room/MatchRoom";
 
-const inter = Inter({ subsets: ["latin"] });
+const Home = (): JSX.Element => {
+  // Firebaseからデータを取ってくる
+  const ROOM_LIST = [
+    { path: "/room1", roomId: 1, joiningList: 1 },
+    { path: "/room2", roomId: 2, joiningList: 3 },
+    { path: "/room3", roomId: 3, joiningList: 0 },
+  ];
 
-export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main className="h-screen">
+      <Container className="h-3/4 flex justify-center items-center">
+        <ul className="flex flex-col sm:flex-row items-center justify-center gap-10">
+          {ROOM_LIST.map((room) => (
+            <li key={room.roomId}>
+              <MatchRoom
+                joiningCount={room.joiningList}
+                path={room.path}
+                room={room.roomId}
+              />
+            </li>
+          ))}
+        </ul>
+      </Container>
     </main>
   );
-}
+};
+
+export default Home;
