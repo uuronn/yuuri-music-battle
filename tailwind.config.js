16385175d3720b2c1227514282d46ceed3980343
uuronn <uuronprogram@gmail.com>
2023-03-31 22:19:38 +0900

    Add: Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  darkMode: false, // 'media' or 'class',
+  purge: {
+    content: [
+      "./app/*.{js,ts,jsx,tsx}",
+      "./app/components/**/*.{js,ts,jsx,tsx}",
+    ],
+    options: {
+      // https://purgecss.com/safelisting.html#patterns
+      safelist: {
+        standard: [/^bg-/, /^text-/],
+      },
+    },
+  },
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
